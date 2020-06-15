bdf1be9c48c92340a3ab24ad0227f1fedbe3f55d
Keith Kintigh <kintigh@asu.edu>
2020-06-15 12:30:56 -0600

    Add files via upload

--- R Files/RoomAccum_Figs.R
+++ R Files/RoomAccum_Figs.R
@@ -6,16 +6,20 @@ library(ggplot2)
 #########################
 #########################
 
-## Imporort data from RoomAccum12.exe program and create figures
+## set up colorblindness friendly color palette
+cbPalette <- c( "#D55E00", "#0072B2", "#F0E442", "#000000", "#56B4E9", "#E69F00", "#009E73", "#CC79A7")
 
 
-# Figure 1 Data, Growth Rates by period for simulated and and compound interest methods of calculation 
+## Import data from RoomAccum12.exe program and create figures
+
+
+# Figure 2 Data, Growth Rates by period for simulated and and compound interest methods of calculation 
 # Variables as follows: input: RoomHesh6.txt, UseLife = 25, S.D. of UseLife = 0.0, InitRooms = 13
-ds.Fig1 <- read.csv('Fig1_HeshRates.csv') 
+ds.Fig2 <- read.csv('Fig2_HeshRates.csv') 
 
-# Plot Figure 1: Estimated growth rates for the Heshotauthla survey derived from the formula and the simulation 
-png("Fig1.png",width=6.5,height=4,units="in",res=256)
-  ds.Fig1 %>%
+# Plot Figure 2: Estimated growth rates for the Heshotauthla survey derived from the formula and the simulation 
+png("Fig2.png",width=6.5,height=4,units="in",res=300)
+  ds.Fig2 %>%
     ggplot(aes(x=Year, y=Rate, group=Approach, color=Approach)) +
     geom_line(size=0.75) +
     scale_x_continuous(name="Year", breaks=seq(900,1275,25)) + 
@@ -26,19 +30,19 @@ png("Fig1.png",width=6.5,height=4,units="in",res=256)
             legend.text=element_text(size=6), 
             legend.title=element_text(size=8)) +
     geom_hline(yintercept = 0) +
-    scale_color_discrete(name = "Growth Rates")
+    scale_colour_manual(values=cbPalette, name = "Growth Rates")
 dev.off()  
 
 #########################
 #########################
 
-# Figure 2 Data, Sensitivity to the number of initial rooms 
+# Figure 3 Data, Sensitivity to the number of initial rooms 
 # Variables as follows: input: RoomHesh6.txt, UseLife = 25, S.D. of UseLife = 0.0, InitRooms = 12, 13, 14
-dp.Fig2 <- read.csv('Fig2_ROOMHESH6_P.CSV')
+dp.Fig3 <- read.csv('Fig3_ROOMHESH6_P.CSV')
 
-# Plot Figure 2: Sensitivity to the number of initial rooms, Heshotauthla survey
-png("Fig2.png",width=6.5,height=4,units="in",res=256)
-  dp.Fig2 %>% 
+# Plot Figure 3: Sensitivity to the number of initial rooms, Heshotauthla survey
+png("Fig3.png",width=6.5,height=4,units="in",res=300)
+  dp.Fig3 %>% 
     ggplot(aes(x=Period, y=Rate, color=as.factor(InitRm))) +
     geom_line(size=0.75) +
     scale_x_continuous(name='Period', breaks=seq(1,6,1)) + 
@@ -48,19 +52,19 @@ png("Fig2.png",width=6.5,height=4,units="in",res=256)
           legend.justification = c(0, 1), 
           legend.text=element_text(size=6), 
           legend.title=element_text(size=8)) +
-    scale_color_discrete(name = "Initial Rooms")
+    scale_colour_manual(values=cbPalette, name = "Initial Rooms")
 dev.off()
 
 #########################
 #########################
 
-# Figure 3 Data, Sensitivity to Room Use-Life 
+# Figure 4 Data, Sensitivity to Room Use-Life 
 # Variables as follows: input: RoomHesh6.txt, UseLife = 15, 20, 25, 30, S.D. of UseLife = 0.0, InitRooms = 8, 10, 13, 15
-dp.Fig3 <- read.csv('Fig3_ROOMHESH6_P.CSV')
+dp.Fig4 <- read.csv('Fig4_ROOMHESH6_P.CSV')
 
-# Plot Figure 3: Sensitivtiy to Room Use-Life, Heshotauthla Survey
-png("Fig3.png",width=6.5,height=4,units="in",res=256)
-dp.Fig3 %>% 
+# Plot Figure 4: Sensitivtiy to Room Use-Life, Heshotauthla Survey
+png("Fig4.png",width=6.5,height=4,units="in",res=300)
+dp.Fig4 %>% 
   ggplot(aes(x=Period, y=Rate, color=as.factor(UseLife))) +
   geom_line(size=0.75) +
   scale_x_continuous(name='Period', breaks=seq(1,6,1)) + 
@@ -70,19 +74,19 @@ dp.Fig3 %>%
         legend.justification = c(0, 1), 
         legend.text=element_text(size=6), 
         legend.title=element_text(size=8)) +
-  scale_color_discrete(name = "Use Life")
+  scale_colour_manual(values=cbPalette, name = "Use Life")
 dev.off()
 
 #########################
 #########################
 
-# Figure 4 Data, Sensitivity to S.D. of Room Use-Life 
+# Figure 5 Data, Sensitivity to S.D. of Room Use-Life 
 # Variables as follows: input: RoomHesh6.txt, UseLife = 25, S.D. of UseLife = 0, 5, 10, 15, 20, InitRooms = 13
-dp.Fig4 <- read.csv('Fig4_ROOMHESH6_P.CSV')
+dp.Fig5 <- read.csv('Fig5_ROOMHESH6_P.CSV')
 
-# Plot Figure 4: Modeled growth rates using normally distributed structure use lives with mean of 25 years and different standard deviations (standard deviation of 0 is a fixed structure use life).
-png("Fig4.png",width=6.5,height=4,units="in",res=256)
-dp.Fig4 %>% 
+# Plot Figure 5: Modeled growth rates using normally distributed structure use lives with mean of 25 years and different standard deviations (standard deviation of 0 is a fixed structure use life).
+png("Fig5.png",width=6.5,height=4,units="in",res=300)
+dp.Fig5 %>% 
   ggplot(aes(x=Period, y=Rate, color=as.factor(UseSD))) +
   geom_line(size=0.75) +
   scale_x_continuous(name='Period', breaks=seq(1,6,1)) + 
@@ -92,20 +96,20 @@ dp.Fig4 %>%
         legend.justification = c(0, 1), 
         legend.text=element_text(size=6), 
         legend.title=element_text(size=8)) +
-  scale_color_discrete(name = "Use Life S.D.")
+  scale_colour_manual(values=cbPalette, name = "Use Life S.D.")
 dev.off()
 
 #########################
 #########################
 
-# Figure 5 Data, Number of Occupied Rooms by approach
+# Figure 6 Data, Number of Occupied Rooms by approach
 # Formula derived as explained in text
 # Simulated derived with Variables as follows: input: RoomHesh6.txt, UseLife = 25, S.D. of UseLife = 0.0, InitRooms = 13
-dp.Fig5 <- read.csv('Fig5_ImpliedDSh.csv')
+dp.Fig6 <- read.csv('Fig6_ImpliedDSh.csv')
 
-# Plot Figure 5: Number of occupied rooms through time for the Heshotauthla survey, computed annually for the simulated and using the formula 
-png("Fig5.png",width=6.5,height=4,units="in",res=256)
-dp.Fig5 %>% 
+# Plot Figure 6: Number of occupied rooms through time for the Heshotauthla survey, computed annually for the simulated and using the formula 
+png("Fig6.png",width=6.5,height=4,units="in",res=300)
+dp.Fig6 %>% 
   ggplot(aes(x=Year, y=OccRm, color=as.factor(Group))) +
   geom_line(size=0.75) +
   scale_x_continuous(name='Year', breaks=seq(900,1275,25)) + 
@@ -115,19 +119,19 @@ dp.Fig5 %>%
         legend.justification = c(0, 1), 
         legend.text=element_text(size=6), 
         legend.title=element_text(size=8)) +
-  scale_color_discrete(name = "Method")
+  scale_colour_manual(values=cbPalette, name = "Method")
 dev.off()
 
 #########################
 #########################
 
-# Figure 6 Data, Growth Rates by period for simulated and and compound interest methods of calculation 
+# Figure 7 Data, Growth Rates by period for simulated and and compound interest methods of calculation 
 # Variables as follows: input: RoomObap.txt, UseLife = 25, S.D. of UseLife = 0.0, InitRooms = 27
-ds.Fig6 <- read.csv('Fig6_OBAPRates.csv')
+ds.Fig7 <- read.csv('Fig6_OBAPRates.csv')
 
-# Plot Figure 6: Estimated growth rates for the Ojo Bonito survey derived from the formula and the simulation 
-png("Fig6.png",width=6.5,height=4,units="in",res=256)
-ds.Fig6 %>%
+# Plot Figure 7: Estimated growth rates for the Ojo Bonito survey derived from the formula and the simulation 
+png("Fig7.png",width=6.5,height=4,units="in",res=300)
+ds.Fig7 %>%
   ggplot(aes(x=Year, y=Rate, group=Approach, color=Approach)) +
   geom_line(size=0.75) +
   scale_x_continuous(name="Year", breaks=seq(1000,1275,25)) + 
@@ -138,20 +142,20 @@ ds.Fig6 %>%
         legend.text=element_text(size=6), 
         legend.title=element_text(size=8)) +
   geom_hline(yintercept = 0) +
-  scale_color_discrete(name = "Growth Rates")
+  scale_colour_manual(values=cbPalette, name = "Growth Rates")
 dev.off()  
 
 #########################
 #########################
 
-## Figure 7 Data, Number of Occupied Rooms by approach
+## Figure 8 Data, Number of Occupied Rooms by approach
 # Formula derived as explained in text
 # Simulated derived with Variables as follows: input: RoomHesh6.txt, UseLife = 25, S.D. of UseLife = 0.0, InitRooms = 27
-dp.Fig7 <- read.csv('Fig7_ImpliedDSo.csv')
+dp.Fig8 <- read.csv('Fig8_ImpliedDSo.csv')
 
-# Plot Figure 7: Number of occupied rooms through time for the Ojo Bonito survey, computed annually for the simulated and using the formula.
-png("Fig7.png",width=6.5,height=4,units="in",res=256)
-dp.Fig7 %>% 
+# Plot Figure 8: Number of occupied rooms through time for the Ojo Bonito survey, computed annually for the simulated and using the formula.
+png("Fig8.png",width=6.5,height=4,units="in",res=300)
+dp.Fig8 %>% 
   ggplot(aes(x=Year, y=OccRm, color=as.factor(Group))) +
   geom_line(size=0.75) +
   scale_x_continuous(name='Year', breaks=seq(1000,1275,25)) + 
@@ -161,5 +165,5 @@ dp.Fig7 %>%
         legend.justification = c(0, 1), 
         legend.text=element_text(size=6), 
         legend.title=element_text(size=8)) +
-  scale_color_discrete(name = "Method")
+  scale_colour_manual(values=cbPalette, name = "Method")
 dev.off()
